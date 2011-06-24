fac32a53c2be5ba461a74184e85e40fd8d92ae6d
Maher Hawash <gmhawash@gmail.com>
2011-06-24 07:22:40 -0700

    Initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zaytona::Application.config.secret_token = '3e120bd75f1c207f1d7192c89360ad35ac9887ea99294c4a366f07264c101e899ab3ad0e490bc7658fcd6194322a0f3726e76b658060389376913ba9c5bf105a'
