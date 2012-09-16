4c2cc036db1600f9876dae33a23eb02e8cc0c744
Maher Hawash <gmhawash@gmail.com>
2012-09-16 13:55:30 -0700

    WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_layout
+
   protect_from_forgery
 
-  layout 'furqan'
+  Layouts = {
+  }
+
+  private
+  def set_layout
+    self.class.layout Layouts[request.subdomain] || request.subdomain
+  end
 end
