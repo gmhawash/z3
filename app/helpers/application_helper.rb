91696c38b33c49352b8c44963dc80e966b9a5a4d
Maher Hawash <gmhawash@gmail.com>
2012-09-17 07:28:12 -0700

    add content page holders for left and right box

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,16 @@ module ApplicationHelper
   def banners
     Image.where("image_name ilike ?", "banner%")
   end
+
+  def home_page_left_box
+    if slug = Slug.find_by_name('home-page-left-box')
+      slug.sluggable.content_for(:body).html_safe
+    end
+  end
+
+  def home_page_right_box
+    if slug = Slug.find_by_name('home-page-right-box')
+      slug.sluggable.content_for(:body).html_safe
+    end
+  end
 end
