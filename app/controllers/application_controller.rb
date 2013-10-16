dacf3811288477bc4a42c8af1cee659df69d6862
Maher Hawash <gmhawash@gmail.com>
2013-10-16 14:16:14 -0700

    force oico layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,8 @@ class ApplicationController < ActionController::Base
   def set_layout
     @layout = 'admin' if request.path =~ /^\/refinery/
     @layout = 'admin' if request.path =~ /^\/users/
+    @layout = 'oico' if request.pat =~ /oico/i
     @layout ||= Layouts[request.subdomain] || request.subdomain
-    self.class.layout @layout 
+    self.class.layout @layout
   end
 end
